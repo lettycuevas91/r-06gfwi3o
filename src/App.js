200e528ccb6dcf972e29dad61405de9b9bcae35b
Leticia <lettycuevas91@gmail.com>
2023-01-16 23:14:02 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const Welcome = (props) =><h1>Hola {props.name} </h1>;
+const Welcome = (props) =><h1 key={props.name}>Hola {props.name} </h1>;
 class App extends Component {
   constructor() {
     super();
-
-    this.state = {
-      names: ["Juan", "Pedro", "Germán"]
-    }
+  
+  this.state = {
+    names: ["Juan", "Pedro", "Germán"]
   }
+}
 
-  render() {
-    
-    return (
-      
-      <div>
-        {this.state.names.map(name =><li>{<Welcome name={name} />}</li>
-        )}
-      </div>
+render() {
+  return (
+    <div>
+      {this.state.names.map((name) =>
+        <Welcome key={name} name={name} />
+      )}
+    </div>
     );
   }
 }
